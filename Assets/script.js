4c9b335da9053568be538a8e6a939f302ec0f26b
Lauren Doss <doss.laurene@gmail.com>
2020-02-20 14:52:00 -0800

    updated variables for buttons and forms

---
+++ Assets/script.js
@@ -0,0 +1,33 @@
+//naming of my global variables
+var dailyInput9 = document.getElementsByClassName("input9"); 
+var dailyInput10 = document.getElementsByClassName("input10"); 
+var dailyInput11 = document.getElementsByClassName("input11"); 
+var dailyInput12 = document.getElementsByClassName("input12"); 
+var dailyInput1 = document.getElementsByClassName("input1"); 
+var dailyInput2 = document.getElementsByClassName("input2"); 
+var dailyInput3 = document.getElementsByClassName("input3"); 
+var dailyInput4 = document.getElementsByClassName("input4"); 
+var dailyInput5 = document.getElementsByClassName("input5"); 
+
+var dailyBtn9 = document.getElementById("inputBtn9"); 
+var dailyBtn10 = document.getElementById("inputBtn10"); 
+var dailyBtn11 = document.getElementById("inputBtn11"); 
+var dailyBtn12 = document.getElementById("inputBtn12"); 
+var dailyBtn1 = document.getElementById("inputBtn1"); 
+var dailyBtn2 = document.getElementById("inputBtn2"); 
+var dailyBtn3 = document.getElementById("inputBtn3"); 
+var dailyBtn4 = document.getElementById("inputBtn4"); 
+var dailyBtn5 = document.getElementById("inputBtn5"); 
+
+//applying a click function to the individual bugttons
+
+
+
+
+// $("#inputBtn9").on("click", function(event) {
+//     event.preventDefault();
+//     var dailyTask9 = document.getElementsByClassName("input9").value
+
+//     localStorage.setItem("dailyTask9", dailyTask9); 
+    
+// })
