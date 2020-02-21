cd9d98c93a216c501b2c1dc9015270bb867dabfe
Lauren Doss <doss.laurene@gmail.com>
2020-02-20 18:47:55 -0800

    updated for loop

--- Assets/script.js
+++ Assets/script.js
@@ -1,13 +1,13 @@
 //naming of my global variables
-var dailyInput9 = document.getElementsByClassName("input9"); 
-var dailyInput10 = document.getElementsByClassName("input10"); 
-var dailyInput11 = document.getElementsByClassName("input11"); 
-var dailyInput12 = document.getElementsByClassName("input12"); 
-var dailyInput1 = document.getElementsByClassName("input1"); 
-var dailyInput2 = document.getElementsByClassName("input2"); 
-var dailyInput3 = document.getElementsByClassName("input3"); 
-var dailyInput4 = document.getElementsByClassName("input4"); 
-var dailyInput5 = document.getElementsByClassName("input5"); 
+var dailyInput9 = document.getElementById("input9"); 
+var dailyInput10 = document.getElementById("input10"); 
+var dailyInput11 = document.getElementById("input11"); 
+var dailyInput12 = document.getElementById("input12"); 
+var dailyInput1 = document.getElementById("input1"); 
+var dailyInput2 = document.getElementById("input2"); 
+var dailyInput3 = document.getElementById("input3"); 
+var dailyInput4 = document.getElementById("input4"); 
+var dailyInput5 = document.getElementById("input5"); 
 
 var dailyBtn9 = document.getElementById("inputBtn9"); 
 var dailyBtn10 = document.getElementById("inputBtn10"); 
@@ -19,9 +19,21 @@ var dailyBtn3 = document.getElementById("inputBtn3");
 var dailyBtn4 = document.getElementById("inputBtn4"); 
 var dailyBtn5 = document.getElementById("inputBtn5"); 
 
-//applying a click function to the individual bugttons
+//applying a click function to the individual buttons
 
-
+dailyBtn9.onclick = function(){
+    const dailyTask9 = {"daily9":dailyInput9.value}
+    console.log({"string9":dailyTask9}); 
+    
+    if (dailyTask9) {
+        localStorage.setItem("dailyTask9",JSON.stringify(dailyTask9)); 
+    }
+}; 
+for (let i = 0; i <localStorage.length; i++) {
+    const dailyTask9 = localStorage.dailyTask9[i]; 
+    const value = localStorage.getItem(dailyTask9); 
+    dailyInput9.innerHTML += `${dailyTask9}: ${value}`; 
+}
 
 
 // $("#inputBtn9").on("click", function(event) {
